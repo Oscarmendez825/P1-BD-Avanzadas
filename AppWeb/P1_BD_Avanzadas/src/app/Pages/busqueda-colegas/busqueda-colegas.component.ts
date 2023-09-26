741582893e006e00f1220822a3505535ffb7a8c5
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2023-09-26 11:11:09 -0600

    [feat]: search components modified

--- AppWeb/P1_BD_Avanzadas/src/app/Pages/busqueda-colegas/busqueda-colegas.component.ts
+++ AppWeb/P1_BD_Avanzadas/src/app/Pages/busqueda-colegas/busqueda-colegas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Investigador } from 'src/app/Interfaces/Investigador';
 
 @Component({
   selector: 'app-busqueda-colegas',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./busqueda-colegas.component.css']
 })
 export class BusquedaColegasComponent {
+  investigadores: Investigador[] = [
+    {
+      id: 1,
+      nombreCompleto: 'Investigador 1',
+      tituloAcademico: 'Doctor en Ciencias',
+      institucion: 'Universidad A',
+      correoElectronico: 'investigador1@example.com',
+    },
+    // Agrega más investigadores según sea necesario
+  ];
 
+  investigadorSeleccionado: Investigador | null = null;
+  investigadoresColegas: Investigador[] = [];
+
+  seleccionarInvestigador(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+  
+    if (selectedValue) {
+      const investigadorSeleccionado = this.investigadores.find(investigador => investigador.nombreCompleto === selectedValue);
+  
+      if (investigadorSeleccionado) {
+        this.investigadorSeleccionado = investigadorSeleccionado;
+        // Simulamos la obtención de las investigaciones del investigador seleccionado.
+        this.investigadoresColegas = [
+          {
+            "id": 1,
+            "nombreCompleto": "Juan Pérez",
+            "tituloAcademico": "Doctor en Ciencias",
+            "institucion": "Universidad XYZ",
+            "correoElectronico": "juan.perez@example.com"
+          },
+          {
+            "id": 2,
+            "nombreCompleto": "María Rodríguez",
+            "tituloAcademico": "Licenciada en Biología",
+            "institucion": "Instituto ABC",
+            "correoElectronico": "maria.rodriguez@example.com"
+          },
+          {
+            "id": 3,
+            "nombreCompleto": "Carlos González",
+            "tituloAcademico": "Máster en Física",
+            "institucion": "Universidad 123",
+            "correoElectronico": "carlos.gonzalez@example.com"
+          }
+          // Agrega más investigaciones según sea necesario
+        ];
+      }
+    }
+  }
 }
--- AppWeb/P1_BD_Avanzadas/src/app/Pages/busqueda-investigador/busqueda-investigador.component.ts
+++ AppWeb/P1_BD_Avanzadas/src/app/Pages/busqueda-investigador/busqueda-investigador.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Investigador } from 'src/app/Interfaces/Investigador';
+import { Project } from 'src/app/Interfaces/Project';
 
 @Component({
   selector: 'app-busqueda-investigador',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class BusquedaInvestigadorComponent {
 
+  investigadores: Investigador[] = [
+    {
+      id: 1,
+      nombreCompleto: 'Investigador 1',
+      tituloAcademico: 'Doctor en Ciencias',
+      institucion: 'Universidad A',
+      correoElectronico: 'investigador1@example.com',
+    },
+    // Agrega más investigadores según sea necesario
+  ];
+
+  investigadorSeleccionado: Investigador | null = null;
+  investigacionesInvestigador: Project[] = [];
+
+  seleccionarInvestigador(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+  
+    if (selectedValue) {
+      const investigadorSeleccionado = this.investigadores.find(investigador => investigador.nombreCompleto === selectedValue);
+  
+      if (investigadorSeleccionado) {
+        this.investigadorSeleccionado = investigadorSeleccionado;
+        // Simulamos la obtención de las investigaciones del investigador seleccionado.
+        this.investigacionesInvestigador = [
+          {
+            titulo: 'Investigación 1',
+            anio: 2020,
+            duracion: 12,
+            area: 'Ciencias',
+          },
+          // Agrega más investigaciones según sea necesario
+        ];
+      }
+    }
+  }
 }

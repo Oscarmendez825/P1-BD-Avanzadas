20386705848c8dce8485c60b3f4b0ae14466b9f3
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2023-09-26 21:04:34 -0600

    [feat]: associate components modified

--- AppWeb/P1_BD_Avanzadas/src/app/Pages/asociar-articulo/asociar-articulo.component.ts
+++ AppWeb/P1_BD_Avanzadas/src/app/Pages/asociar-articulo/asociar-articulo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Project } from 'src/app/Interfaces/Project';
+import { Publicacion } from 'src/app/Interfaces/Publicacion';
 
 @Component({
   selector: 'app-asociar-articulo',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AsociarArticuloComponent {
 
+  articulos: Publicacion[] = [
+    {
+      "titulo": "Investigación sobre Cambio Climático",
+      "anio": 2022,
+      "revista": "Nature"
+    },
+    {
+      "titulo": "Estudio de Energía Renovable",
+      "anio": 2023,
+      "revista": "Science"
+    },
+    {
+      "titulo": "Desarrollo de Inteligencia Artificial",
+      "anio": 2024,
+      "revista": "Journal of Artificial Intelligence Research"
+    },
+    {
+      "titulo": "Proyecto de Ciencia de Datos",
+      "anio": 2022,
+      "revista": "Data Science Journal"
+    },
+    {
+      "titulo": "Investigación en Medicina Genómica",
+      "anio": 2023,
+      "revista": "Genome Research"
+    }
+  ];
+  proyectos: Project[] = [
+    {
+      titulo: 'Proyecto 1',
+      anio: 2023,
+      duracion: 2,
+      area: 'Ciencia'
+    },
+    // Agrega más proyectos aquí
+  ];
+  selectedArticulo: string | undefined;
+  selectedProyecto: string | undefined;
+  isButtonDisabled: boolean = true; // Inicialmente, el botón estará deshabilitado
+
+
+  checkFormValidity(): void {
+    this.isButtonDisabled = !(this.selectedArticulo && this.selectedProyecto);
+  }
+  afiliarInvestigador(): void {
+    if (this.selectedArticulo && this.selectedProyecto) {
+      console.log(`Se afilió al investigador ${this.selectedArticulo} al proyecto ${this.selectedProyecto}`);
+    }
+  }
 }
--- AppWeb/P1_BD_Avanzadas/src/app/Pages/asociar-investigador/asociar-investigador.component.ts
+++ AppWeb/P1_BD_Avanzadas/src/app/Pages/asociar-investigador/asociar-investigador.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Investigador } from 'src/app/Interfaces/Investigador';
+import { Project } from 'src/app/Interfaces/Project';
 
 @Component({
   selector: 'app-asociar-investigador',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./asociar-investigador.component.css']
 })
 export class AsociarInvestigadorComponent {
+  investigadores: Investigador[] = [
+    {
+      id: 1,
+      nombreCompleto: 'Investigador 1',
+      tituloAcademico: 'PhD',
+      institucion: 'Universidad A',
+      correoElectronico: 'investigador1@example.com'
+    },
+    // Agrega más investigadores aquí
+  ];
+
+  proyectos: Project[] = [
+    {
+      titulo: 'Proyecto 1',
+      anio: 2023,
+      duracion: 2,
+      area: 'Ciencia'
+    },
+    // Agrega más proyectos aquí
+  ];
+  selectedInvestigador: number | undefined;
+  selectedProyecto: string | undefined;
+  isButtonDisabled: boolean = true; // Inicialmente, el botón estará deshabilitado
+
+
+  checkFormValidity(): void {
+    this.isButtonDisabled = !(this.selectedInvestigador && this.selectedProyecto);
+  }
+  afiliarInvestigador(): void {
+    if (this.selectedInvestigador && this.selectedProyecto) {
+      console.log(`Se afilió al investigador ${this.selectedInvestigador} al proyecto ${this.selectedProyecto}`);
+    }
+  }
 
 }

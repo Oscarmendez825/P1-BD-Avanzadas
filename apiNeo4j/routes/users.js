6ee6074d48dfdeefdbfc23ca5e92dc6d0c98aaa7
BerthaBrenes <bertacaro1996@gmail.com>
2023-09-30 17:35:58 -0600

    [error}neo4j

--- apiNeo4j/routes/users.js
+++ apiNeo4j/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const graphDBConnect = require('../middleware/graphDBConnect');
 
+function formatResponse(resultObj) {
+  const result = [];
+  if (resultObj.records.length > 0) {
+    resultObj.records.map(record => {
+      result.push(record._fields[0].properties);
+    });
+  }
+  return result;
+}
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res) {
+  const query = 'MATCH (x) RETURN x';
+  const params = {};
+  const resultObj = await graphDBConnect.executeCypherQuery(query, params);
+  const result = formatResponse(resultObj);
+  res.send(result);
 });
 
 module.exports = router;
